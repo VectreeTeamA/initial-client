85417f047f3ebc838ca0e8600a9b8595f281f325
PolinaShneider <beatrixlinkoln@gmail.com>
2019-01-29 14:43:33 +0300

    Use async instead of promise callbacks

--- src/store/actions/authAction.js
+++ src/store/actions/authAction.js
@@ -1,43 +1,45 @@
 import axios from "axios";
 
-export const loginUser = user => (dispatch, getState) => {
+export const loginUser = user => async (dispatch, getState) => {
   const { userName, password } = user;
 
-  axios
-    .post("/api/auth/login", {
+  try {
+    const result = await axios.post("/api/auth/login", {
       username: userName,
       password: password
-    })
-    .then(result => {
-      dispatch({
-        type: "LOGIN",
-        payload: {
-          userName: userName,
-          password: password,
-          result: result.data.message
-        }
-      });
-    })
-    .catch(error => console.error(error));
+    });
+
+    dispatch({
+      type: "LOGIN",
+      payload: {
+        userName: userName,
+        password: password,
+        result: result.data.message
+      }
+    });
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-export const registerUser = user => (dispatch, getState) => {
+export const registerUser = user => async (dispatch, getState) => {
   const { userName, password } = user;
 
-  axios
-    .post("/api/auth/register", {
+  try {
+    const result = await axios.post("/api/auth/register", {
       username: userName,
       password: password
-    })
-    .then(result => {
-      dispatch({
-        type: "REGISTER",
-        payload: {
-          userName: userName,
-          password: password,
-          result: result.data.message
-        }
-      });
-    })
-    .catch(error => console.error(error));
+    });
+
+    dispatch({
+      type: "REGISTER",
+      payload: {
+        userName: userName,
+        password: password,
+        result: result.data.message
+      }
+    });
+  } catch (error) {
+    console.error(error);
+  }
 };
